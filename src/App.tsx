a3eb297e3e70214e69e3f4e9c121ea001912914a
Eric Cote <eric@coteexpert.com>
2023-04-20 08:29:39 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import '~bootstrap/dist/css/bootstrap.css';
-
 import './App.css';
 import SlidesEn from './slides.en.mdx';
 import SlidesFr from './slides.fr.mdx';
@@ -8,7 +7,7 @@ import ThemeProvider from './components/dark-mode/ThemeProvider';
 export default function App() {
   const lang = location.pathname === '/fr' ? 'fr' : 'en';
   return (
-    <ThemeProvider>
+    <ThemeProvider theme='auto'>
       <article>{lang === 'fr' ? <SlidesFr /> : <SlidesEn />}</article>
     </ThemeProvider>
   );
