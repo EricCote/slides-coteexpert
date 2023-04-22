5ae007fcf736bf5e9be07464fdd6ac40c867a443
Eric Cote <eric@coteexpert.com>
2023-04-22 13:20:45 -0400

    slides changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import '~bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import SlidesEn from './slides.en.mdx';
-import SlidesFr from './slides.fr.mdx';
+import SlidesEn from './decks/slides.en.mdx';
+import SlidesFr from './decks/slides.fr.mdx';
 import ThemeProvider from './components/dark-mode/ThemeProvider';
 import GotoPopup from './components/GotoPopup';
 
