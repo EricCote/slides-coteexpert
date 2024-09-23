2a8a395ae5d875699d4d8415b66a689c0a6c53b1
Eric Cote <eric@coteexpert.com>
2024-09-23 05:26:02 -0400

    deploy

--- src/decks/React/15-patterns/cat.jsx
+++ src/decks/React/15-patterns/cat.jsx
@@ -1,8 +1,9 @@
 export default `export default function Cat({ mouse }) {
   return (
     <img
-      src='/cat.jpg'
+      src='https://cdn5.vectorstock.com/i/1000x1000/28/54/face-cat-fluffy-lovely-animal-outline-vector-12722854.jpg'
       alt='cat'
+       width="150"
       style={{ position: 'absolute', left: mouse.x, top: mouse.y }}
     />
   );
